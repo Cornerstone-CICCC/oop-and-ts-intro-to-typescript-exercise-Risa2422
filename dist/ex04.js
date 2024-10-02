26dba34141424d65827dbed1401e2bc0a9e6d354
Risa Yamamoto <yamari2422@gmail.com>
2024-10-02 16:17:09 -0700

    Finished exercise4

--- dist/ex04.js
+++ dist/ex04.js
@@ -3,6 +3,13 @@
 //    The function should return the largest number in the array.
 //    Ensure the parameter and return type are typed.
 function findMax(numbers) {
+    let largestNum = 0;
+    numbers.map((num) => {
+        if (num > largestNum) {
+            largestNum = num;
+        }
+    });
+    return largestNum;
 }
 // Expected output:
 console.log(findMax([1, 5, 3, 9, 2])); // 9
--- src/ex04.ts
+++ src/ex04.ts
@@ -2,10 +2,18 @@
 //    The function should return the largest number in the array.
 //    Ensure the parameter and return type are typed.
 
-function findMax(numbers) {
+function findMax(numbers: number[]): number {
+  let largestNum: number = 0;
 
+  numbers.map((num) => {
+    if (num > largestNum) {
+      largestNum = num;
+    }
+  });
+
+  return largestNum;
 }
 
 // Expected output:
 console.log(findMax([1, 5, 3, 9, 2])); // 9
-console.log(findMax([7, 2, 10, 4]));   // 10
+console.log(findMax([7, 2, 10, 4])); // 10
